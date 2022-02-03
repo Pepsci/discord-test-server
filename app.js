c61e3523e6a4703124c181e1048c4fc126f3fe87
Nicolas Casabianca <nicolas_casabianca@hotmail.fr>
2022-02-03 18:08:13 +0100

    sfr m'a tué

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 require("dotenv").config();
 require("./config/mongoDb");
 const express = require("express");
+const app = express();
+
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const http = require("http")
+const server = http.createServer(app);
+// const { Server } = require("socket.io");
+// const io = new Server(server);
+const io = require("socket.io")(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+      credentials: true
+    }
+  });
+
 
 const authRouter = require("./routes/auth");
 const { isAuthenticated } = require("./middlewares/jwt.middleware");
-
-const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -17,5 +29,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", authRouter);
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+});
 
 module.exports = app;
