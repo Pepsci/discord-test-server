3be8c411c49c2f2dccd2f0ef8b4161c62817da03
Armelle Loiseau <armelle.loiseau@gmail.com>
2022-02-10 00:02:58 +0100

    new route in chan to get one  chan's info

--- routes/chan.js
+++ routes/chan.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const chanModel = require("./../models/chan.model");
-const Message = require("../models/message.model")
+const Message = require("../models/message.model");
 
 // this route is prefixed with chan !!
 
@@ -25,17 +25,26 @@ router.post("/", async (req, res, next) => {
   }
 });
 
+router.get("/:id", async (req, res, next) => {
+  try {
+    console.log(req.params.id);
+    const chan = await chanModel.findById(req.params.id).populate("owner");
+    res.status(200).json(chan);
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.get("/:id/messages", async (req, res, next) => {
   try {
-    const prevMessages = await Message
-          .find({ chan: req.params.id })
-          .populate("author");
-    res.status(200).json(prevMessages)
-  }
-catch(e){
-  res.status(500).json({ message: "Internal Server Error" });
+    const prevMessages = await Message.find({ chan: req.params.id }).populate(
+      "author"
+    );
+    res.status(200).json(prevMessages);
+  } catch (e) {
+    res.status(500).json({ message: "Internal Server Error" });
     next(e);
-}
+  }
 });
 
 module.exports = router;
