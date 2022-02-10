d0f5014ed1fa51ee6d9dae0390a820965a6e3257
Armelle Loiseau <armelle.loiseau@gmail.com>
2022-02-10 14:09:15 +0100

    finished chan route

--- routes/chan.js
+++ routes/chan.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const chanModel = require("./../models/chan.model");
 const Message = require("../models/message.model");
+const uploader = require("./../config/cloudinary");
 
 // this route is prefixed with chan !!
 
@@ -16,8 +17,10 @@ router.get("/", async (req, res, next) => {
 });
 
 router.post("/", async (req, res, next) => {
+  const { name, owner } = req.body;
+  const image = `https://avatars.dicebear.com/api/identicon/${name}.svg`;
   try {
-    const newChan = await chanModel.create(req.body);
+    const newChan = await chanModel.create({ name, owner, image });
     res.status(201).json(newChan);
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" });
@@ -30,6 +33,30 @@ router.get("/:id", async (req, res, next) => {
     console.log(req.params.id);
     const chan = await chanModel.findById(req.params.id).populate("owner");
     res.status(200).json(chan);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch("/:id", uploader.single("image"), async (req, res, next) => {
+  const image = req.file?.path || undefined;
+
+  try {
+    const updateChan = await chanModel.findByIdAndUpdate(
+      req.params.id,
+      { ...req.body, image },
+      { new: true }
+    );
+    res.status(200).json(updateChan);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const deleteChan = await chanModel.findByIdAndDelete(req.params.id);
+    res.status(200).json(deleteChan);
   } catch (error) {
     next(error);
   }
